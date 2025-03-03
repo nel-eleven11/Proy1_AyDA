3c4847c5d1b2bd6f7dcb073979d141759bc7d58c
Its-Japo <pue22296@uvg.edu.gt>
2025-03-02 22:21:48 -0600

    Integración de profiler y optimización del menú

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from parser_yaml import cargar_configuracion, validar_configuracion, procesar_co
 from mt import simular_mt
 from utils import mostrar_ids
 import os
+import cProfile
+import pstats
 
 def main():
 
@@ -12,148 +14,63 @@ def main():
     print("Nelson García 22434")
     print("Joaquín Puente 22296\n")
     
-    while True:
-        print("Máquinas de Turing Reconocedoras")
-        print("Seleccione la Máquina de Turing a simular:")
-        print("1. Probar MT de Fibonacci")
-        print("0. Salir")
-        
-        seleccion = input("Ingrese el número de la opción deseada: ").strip()
+    print("Máquinas de Turing Reconocedoras")
 
+    # Definir las rutas de los archivos YAML para cada máquina
+    yaml_files = {
+        '1': 'mt1.yaml',
+    }
+    
+    archivo_yaml = yaml_files.get('1')
+    
+    if not archivo_yaml:
+        print("Archivo YAML no definido para la selección.\n")
+        exit(1)    
+    # Verificar que el archivo YAML existe
+    if not os.path.isfile(archivo_yaml):
+        print(f"Error: El archivo YAML '{archivo_yaml}' no fue encontrado.\n")
+        exit(1) 
 
-        if seleccion == '0':
-            print("Saliendo del programa. ¡Hasta luego!")
-            break
-        elif seleccion not in ['1']:
-            print("Selección inválida. Por favor, ingrese 1 o 0.\n")
-            continue
+    try:
+        # Cargar y procesar la configuración
+        config = cargar_configuracion(archivo_yaml)
+        validar_configuracion(config)
+        configuracion_procesada = procesar_configuracion(config)
 
-        # Definir las rutas de los archivos YAML para cada máquina
-        yaml_files = {
-            '1': 'mt1.yaml',
-        }
-        
-        archivo_yaml = yaml_files.get(seleccion)
-        
-        if not archivo_yaml:
-            print("Archivo YAML no definido para la selección.\n")
-            continue
-        
-        # Verificar que el archivo YAML existe
-        if not os.path.isfile(archivo_yaml):
-            print(f"Error: El archivo YAML '{archivo_yaml}' no fue encontrado.\n")
-            continue
-        
-        try:
-            # Cargar y procesar la configuración
-            config = cargar_configuracion(archivo_yaml)
-            validar_configuracion(config)
-            configuracion_procesada = procesar_configuracion(config)
+        # Menú para seleccionar categoría de cadenas basado en la máquina seleccionada
+        while True:
 
-            # Menú para seleccionar categoría de cadenas basado en la máquina seleccionada
-            while True:
+            enesimo = int(input('Ingrese el n número que desea de Fibbonacci (0 para salir):\n'))
+            if enesimo == 0:
+                print('Saliendo del programa')
+                exit(0)
 
-#                print("\nCategorías de Cadenas:")
-#                
-#                # Definir categorías según la máquina seleccionada
-#                if seleccion == '1':
-#                    print("1. Cadenas ACEPTADAS")
-#                    print("2. Cadenas RECHAZADAS")
-#                    print("3. Simular ambas categorías")
-#                elif seleccion == '2':
-#                    print("1. Cadenas ACEPTADAS")
-#                    print("2. Simular todas las cadenas ACEPTADAS")
-#                
-#                print("0. Volver al menú principal")
-#                
-#                categoria = input("Ingrese el número de la categoría deseada: ").strip()
-#
-#                if categoria == '0':
-#                    print()
-#                    break
-#                elif seleccion == '1' and categoria not in ['1', '2', '3']:
-#                    print("Selección inválida. Por favor, ingrese 1, 2, 3 o '0'.\n")
-#                    continue
-#                elif seleccion == '2' and categoria not in ['1', '2']:
-#                    print("Selección inválida. Por favor, ingrese 1, 2 o '0'.\n")
-#                    continue
-#
-#                # Obtener las cadenas según la categoría seleccionada
-#                if seleccion == '1':
-#                    if categoria == '1':
-#                        cadenas = config['accepted_strings']
-#                        categoria_texto = "Cadenas ACEPTADAS"
-#                    elif categoria == '2':
-#                        cadenas = config['rejected_strings']
-#                        categoria_texto = "Cadenas RECHAZADAS"
-#                    elif categoria == '3':
-#                        cadenas = config['accepted_strings'] + config['rejected_strings']
-#                        categoria_texto = "Cadenas ACEPTADAS y RECHAZADAS"
-#                elif seleccion == '2':
-#                    if categoria == '1':
-#                        cadenas = config['accepted_strings']
-#                        categoria_texto = "Cadenas ACEPTADAS"
-#                    elif categoria == '2':
-#                        cadenas = config['accepted_strings']
-#                        categoria_texto = "Cadenas ACEPTADAS"
-#                        # La opción 2 para la Máquina 2 es simular todas las cadenas aceptadas
-#
-#                if not cadenas:
-#                    print(f"No hay cadenas definidas en la categoría '{categoria_texto}'.\n")
-#                    continue
-#
-#                print(f"\n{categoria_texto}:")
-#                for idx, cadena in enumerate(cadenas, start=1):
-#                    print(f"{idx}. '{cadena}'")
-#                
-#                # Definir las opciones de selección de cadenas basadas en la máquina
-#                if seleccion == '1':
-#                    print("0. Simular todas las cadenas de la categoría")
-#                elif seleccion == '2':
-#                    print("0. Simular todas las cadenas ACEPTADAS")
-#                
-#                seleccion_cadenas = input("Seleccione las cadenas a simular (ej. 1,3,5) o '0' para todas: ").strip()
+            cadena_sim = ''
+            for i in range(enesimo):
+                cadena_sim += '1' 
 
-#                if seleccion_cadenas == '0':
-#                    cadenas_seleccionadas = cadenas
-#                else:
-#                    indices = seleccion_cadenas.split(',')
-#                    try:
-#                        indices = [int(idx.strip()) for idx in indices]
-#                        # Para Máquina 2, aunque la opción 2 también selecciona aceptadas,
-#                        # nos aseguramos de que solo se seleccionen aceptadas.
-#                        if seleccion == '1' and categoria == '3':
-#                            # En caso de seleccionar ambas categorías, no hay duplicados
-#                            # simplemente selecciona todas las cadenas.
-#                            cadenas_seleccionadas = [cadenas[idx - 1] for idx in indices if 1 <= idx <= len(cadenas)]
-#                        else:
-#                            cadenas_seleccionadas = [cadenas[idx - 1] for idx in indices if 1 <= idx <= len(cadenas)]
-#                        if not cadenas_seleccionadas:
-#                            print("No se seleccionaron cadenas válidas.\n")
-#                            continue
-#                    except (ValueError, IndexError):
-#                        print("Entrada inválida. Por favor, ingrese números separados por comas o '0'.\n")
-#                        continue
-                enesimo = int(input('Ingrese el n número que desea de Fibbonacci:\n'))
-                cadena_sim = ''
-                for i in range(enesimo):
-                    cadena_sim += '1' 
+            # Simular las cadenas seleccionadas
+            # for cadena in cadenas_seleccionadas:
+            print(f"\nSimulando cadena: '{cadena_sim}'")
+            profiler = cProfile.Profile()
+            profiler.enable()
+            ids, aceptada = simular_mt(configuracion_procesada, cadena_sim)
+            profiler.disable()
+            mostrar_ids(ids)
+            if aceptada:
+                print("Resultado: La cadena es ACEPTADA.")
+            else:
+                print("Resultado: La cadena es RECHAZADA.")
+            print('=' * 50)
+            print('===============================================')
+            print(f'EL N-ESIMO NÚMERO DE FIBBONACCI: {ids[-1]['cinta'].count('1')}')
+            stats = pstats.Stats(profiler).sort_stats('cumulative')
+            stats.print_stats()
 
-                # Simular las cadenas seleccionadas
-                # for cadena in cadenas_seleccionadas:
-                print(f"\nSimulando cadena: '{cadena_sim}'")
-                ids, aceptada = simular_mt(configuracion_procesada, cadena_sim)
-                mostrar_ids(ids)
-                if aceptada:
-                    print("Resultado: La cadena es ACEPTADA.")
-                else:
-                    print("Resultado: La cadena es RECHAZADA.")
-                print('=' * 50)
-                print('===============================================')
-                print(f'EL N-ESIMO NÚMERO DE FIBBONACCI: {ids[-1]['cinta'].count('1')}')
-        
-        except Exception as e:
-            print(f"Error: {e}\n")
+    except Exception as e:
+        print(f"Error: {e}\n")
 
 if __name__ == '__main__':
+
     main()
+
